72070ff3f382adf72a5cc2dd47a33cac9aded921
hp <hp@LAPTOP-H03TBVBI>
2022-08-20 13:35:49 +0530

    Add project files.

---
+++ FoodDonation/SQLQuery1.sql
@@ -0,0 +1,6 @@
+insert into UserMaster values('admin','kuppili','madhuri',' 2001-09-25 00:00:00','female', '1234567891','kuppilimadhuri2gmail.com','','123','1'),
+('user','kotagiri','annu','2001-7-18 00:00:00','male','1234567892','kotagiriannu@gmail.com',''
+,'456','2')
+select*from UserMaster
+select*from LogisticRequest
+select*from LogisticDonationRequest
